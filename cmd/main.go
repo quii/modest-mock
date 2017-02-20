f64b5b7e53f1608379fc8eea19801840258b2bc3
Chris James <qui666@gmail.com>
2017-02-20 09:46:20 +0000

    just some spikey code

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+// https://blog.gopheracademy.com/code-generation-from-the-ast/
+
+func main() {
+	src := `
+package main
+type Store interface{
+	Save(firstname, lastname) (err error)
+}
+`
+
+	// Create the AST by parsing src.
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "src.go", src, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// Inspect the AST and print all identifiers and literals.
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.InterfaceType:
+			fmt.Println(x.Interface)
+			for _, method := range x.Methods.List {
+				fmt.Println(method)
+
+				for _, x := range method.Names {
+					fmt.Println(x)
+				}
+			}
+		}
+		return true
+	})
+}
