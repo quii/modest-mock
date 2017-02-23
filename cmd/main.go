c4a0c6d253f215ff8f8ad8798e3c3327f028d226
Chris James <qui666@gmail.com>
2017-02-23 09:35:08 +0000

    pull spikey code from main

--- cmd/main.go
+++ cmd/main.go
@@ -2,46 +2,11 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 )
 
 // https://blog.gopheracademy.com/code-generation-from-the-ast/
 
 func main() {
-	src := `
-package main
-type Store interface{
-	Save(firstname, lastname string) (err error)
-}
-`
+	fmt.Println("Hello, world")
 
-	// Create the AST by parsing src.
-	fset := token.NewFileSet() // positions are relative to fset
-	f, err := parser.ParseFile(fset, "src.go", src, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	// Inspect the AST and print all identifiers and literals.
-	ast.Inspect(f, func(n ast.Node) bool {
-		switch x := n.(type) {
-		case *ast.InterfaceType:
-			fmt.Println(x.Interface)
-			for _, method := range x.Methods.List {
-				fmt.Println(method)
-
-				for _, x := range method.Names {
-					fmt.Println(x)
-				}
-			}
-		case *ast.FieldList:
-			fmt.Println("number of fields", x.NumFields())
-			for _, field := range x.List {
-				fmt.Println(field)
-			}
-		}
-		return true
-	})
 }
--- mock.go
+++ mock.go
@@ -1,6 +1,12 @@
 package modest_mock
 
-import "io"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+)
 
 type Value struct {
 	Name, Type string
@@ -16,6 +22,35 @@ type Mock struct {
 	Methods map[string]Method
 }
 
-func New(src io.Reader, name string) (Mock, error) {
-	return Mock{}, nil
+func New(src io.Reader, name string) (mock Mock, err error) {
+
+	// Create the AST by parsing src.
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "src.go", src, 0)
+	if err != nil {
+		return
+	}
+
+	// Inspect the AST and print all identifiers and literals.
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.InterfaceType:
+			fmt.Println(x.Interface)
+			for _, method := range x.Methods.List {
+				fmt.Println(method)
+
+				for _, x := range method.Names {
+					fmt.Println(x)
+				}
+			}
+		case *ast.FieldList:
+			fmt.Println("number of fields", x.NumFields())
+			for _, field := range x.List {
+				fmt.Println(field)
+			}
+		}
+		return true
+	})
+
+	return
 }
