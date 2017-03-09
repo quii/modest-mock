611eef94f254cd3d052dcb723d480fc0570dd6e8
Chris James <qui666@gmail.com>
2017-03-09 13:51:14 +0000

    tinkering

--- example/ATM_test.go
+++ example/ATM_test.go
@@ -2,44 +2,46 @@ package example
 
 import "testing"
 
-func TestATM_NewSession(t *testing.T) {
+const success = true
+const fail = false
 
+func TestATM_NewSession_Checks_Pin(t *testing.T) {
+
+	// inject the mocked bank into the ATM that we're testing
 	bank := NewBankMock()
+	atm := ATM{bank}
 
-	t.Run("it goes to the bank to check the users pin before starting a session", func(t *testing.T) {
+	accountNumber := 12345678
+	cardNumber := 12345677
+	pin := 9999
 
-		accountNumber := 12345678
-		cardNumber := 12345677
-		pin := 9999
+	// set up mock. the key of the map are your args to CheckPin, the values are.. your values!
+	bank.Returns.CheckPin = BankMock_CheckPinReturnsMap{
+		{cardNumber, pin}: {accountNumber, success},
+		{123123, 5678}:    {0, fail},
+	}
 
-		// set up mock to return account number when sent correct card number and pin
-		bank.Returns.CheckPin = BankMock_CheckPinReturnsMap{
-			{cardNumber, pin}: {accountNumber, true},
-		}
+	// call the method you want to test
+	session, err := atm.NewSession(cardNumber, pin)
 
-		atm := ATM{bank}
+	// make assertions
+	if err != nil {
+		t.Fatal("Didnt expect it to fail")
+	}
 
-		session, err := atm.NewSession(cardNumber, pin)
+	if session == nil {
+		t.Fatal("expected a session")
+	}
 
-		if err != nil {
-			t.Fatal("Didnt expect it to fail")
-		}
+	if session.accountNumber != accountNumber {
+		t.Error("Account number was not set correctly")
+	}
 
-		if session == nil {
-			t.Fatal("expected a session")
-		}
+	if bank.Calls.CheckPin[0].cardNumber != cardNumber {
+		t.Error("Bank was not called with correct card number")
+	}
 
-		if session.accountNumber != accountNumber {
-			t.Error("Account number was not set correctly")
-		}
-
-		if bank.Calls.CheckPin[0].cardNumber != cardNumber {
-			t.Error("Bank was not called with correct card number")
-		}
-
-		if bank.Calls.CheckPin[0].pin != pin {
-			t.Error("Bank was not called with correct pin")
-		}
-
-	})
+	if bank.Calls.CheckPin[0].pin != pin {
+		t.Error("Bank was not called with correct pin")
+	}
 }
